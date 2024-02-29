b7a616a5ce273c953d1e482d1e22ed5541a348e9
Demarquies Jolley <ahmir1128@gmail.com>
2024-02-28 20:35:04 -0600

    index page

--- location.js
+++ location.js
@@ -1,15 +1,55 @@
-// Call the function to display city information list
-// displayCityInfoList();
 
-// Constructor function for salmon cookie stand
-function CityHours(city, minCustomers, maxCustomers, avgSale) {
-  this.name = name;
-  this.minCustomers = minCustomers;
-  this.maxCustomers = maxCustomers;
-  this.avgSale = avgSale;
-  this.CookiesSoldEachHour = [];
-  this.dailyStoreTotal = 0;
+function CookieLocation(city, hours, contact, location) {
+  this.city = city;
+  this.hours = hours;
+  this.contact = contact;
+  this.location = location;
 }
+
+// Create instances for each city
+var seattle = new CookieLocation('Seattle', '6am - 7pm', '2901 3rd Ave #300, Seattle,WA 98121', [123-456-7890]);
+var tokyo = new CookieLocation('Tokyo', '6am - 7pm', '1Chrome 1-2 Oshiage, Sumida City, Tokyo 131-8634', [222-222-2222]);
+var dubai = new CookieLocation('Dubai', '6am - 7pm', '1 Sheikh Mohammadbin Rashid Blvd-Dubai', [333-333-3333]);
+var paris = new CookieLocation('Paris', '6am - 7pm', 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris', [444-444-4444]);
+var lima = new CookieLocation('Lima', '6am - 7pm', 'Ca. Gral Borgono cuandra 8, Miraflores 15074', [555-555-5555]);
+
+// Print locations on the page
+function printLocations(locations) {
+  var output = document.getElementById('output');
+  locations.forEach(function (location) {
+    var div = document.createElement('div');
+    div.innerHTML = `<h3>${location.city}</h3>
+                     <p>Hours: ${location.hours}</p>
+                     <p>Contact: ${location.contact}</p>
+                     <p>Location: ${location.location.join(', ')}</p>`;
+    output.appendChild(div);
+  });
+}
+
+// Array of locations
+var locations = [seattle, tokyo, dubai, paris, lima];
+
+// Call the printLocations function with the array
+printLocations(locations);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function displayCityInfo(location) {
