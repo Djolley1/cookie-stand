80a6b3b7b2119b0b08f812fa1ddcf40ca65bfdb2
Demarquies Jolley <ahmir1128@gmail.com>
2024-02-28 19:46:41 -0600

    added footer

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
  
 let salesData = document.getElementById("sales-data")
 let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"]
+let totalsPerHour = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let totalSales = 0;
+const allCookieStands = []
 // Constructor function for salmon cookie stand
 function CookieStand(name, minCustomers, maxCustomers, avgSale) {
     this.name = name;
     this.minCustomers = minCustomers;
     this.maxCustomers = maxCustomers;
     this.avgSale = avgSale;
-    this.CookiesSoldEachHour = []
+    this.CookiesSoldEachHour = [];
     this.dailyStoreTotal = 0;
 }
 
@@ -16,6 +19,8 @@ let tokyoStore = new CookieStand("Tokyo", 3, 24, 1.2)
 let dubaiStore = new CookieStand("dubai", 11, 38, 3.7)
 let parisStore = new CookieStand("Paris", 20, 38, 2.3)
 let limaStore = new CookieStand("Lima", 2, 16, 4.6)
+allCookieStands.push(seattleStore, tokyoStore, dubaiStore, parisStore, limaStore);
+console.log(allCookieStands)
 
 
 // Method to simulate cookies purchased
@@ -27,6 +32,7 @@ CookieStand.prototype.simulateCookiesPurchased =function()
         let cookiesSold = Math.round(customer * this.avgSale);
         this.CookiesSoldEachHour.push(cookiesSold);
         this.dailyStoreTotal += cookiesSold;
+        totalSales += cookiesSold;
     };
 } 
 
@@ -81,6 +87,9 @@ CookieStand.prototype.renderStoreRow = function() {
         cell = document.createElement("td");
         cell.textContent = this.CookiesSoldEachHour[i];
         row.appendChild(cell);
+        totalsPerHour[i] += this.CookiesSoldEachHour[i]
+        console.log(totalsPerHour[i])
+        console.log(this.CookiesSoldEachHour[i])
     }
 
     cell = document.createElement("td");
@@ -97,6 +106,39 @@ tokyoStore.renderStoreRow();
 dubaiStore.renderStoreRow();
 parisStore.renderStoreRow();
 limaStore.renderStoreRow();
+
+
+function createTableFooter() {
+    let footer = document.getElementById("sales-data");
+    let row = document.createElement("tr");
+    footer.appendChild(row);
+
+    let totalLabel = document.createElement("td");
+    totalLabel.textContent = "Hours Total";
+    row.appendChild(totalLabel);
+
+    for(let i=0; i<totalsPerHour.length; i++) {
+        console.log(totalsPerHour)
+        let td = document.createElement("td");
+        td.textContent = totalsPerHour[i];
+        row.appendChild(td);
+        totalSales += totalsPerHour[i]
+    }
+    let mainTotal = document.createElement("td");
+    mainTotal.textContent = `${totalSales}`;
+    row.appendChild(mainTotal);
+    
+}
+
+function start() {
+    console.log("Starting Footer");
+}
+
+createTableFooter()
+
+
+
+
 
 // Display results for each location
 // displayCookiesPerHour(seattle);
